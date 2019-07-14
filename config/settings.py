e9a6583a7dd78c8d1f460878c0eb516553012215
Bartosz Niczyporuk <bartosz.niczyporuk781@gmail.com>
2019-07-14 17:45:40 +0200

    testing with sqlite

--- config/settings.py
+++ config/settings.py
@@ -93,11 +93,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
+}
+"""
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:',
+  }
 }
 
 CACHES = {
