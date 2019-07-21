1d94235c5e6954f78305f9109ce62304ee82f605
Bounty1993 <bartosz.niczyporuk781@gmail.com>
2019-07-21 22:28:54 +0200

    Delete maker_data.py

--- maker_data.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from my_wallet.stocks.utils import StockMaker
-
-with open('my_wallet/stocks/fixtures.txt') as file:
-    for ticker in file:
-        StockMaker(ticker.strip()).add_all()
